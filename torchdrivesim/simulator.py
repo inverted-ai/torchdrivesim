fd994992e493cde142dc93375a8c5f3f28f684ee
rlyu <ruishenlyu@gmail.com>
2024-03-05 12:39:35 -0800

    Use default factory for mutable default

--- torchdrivesim/simulator.py
+++ torchdrivesim/simulator.py
@@ -1,7 +1,7 @@
 import abc
 import logging
 import os
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from enum import Enum
 from itertools import accumulate
 from typing import Optional, Union, Dict, List, Iterable, Callable, Any
@@ -43,9 +43,9 @@ class TorchDriveConfig:
     """
     Top-level configuration for a TorchDriveSim simulator.
     """
-    renderer: RendererConfig = RendererConfig()  #: how to visualize the world, for the user and for the agents
+    renderer: RendererConfig = field(default_factory=lambda:RendererConfig())  #: how to visualize the world, for the user and for the agents
     single_agent_rendering: bool = False  #: if set, agents don't see each other
-    collision_metric: CollisionMetric = CollisionMetric.discs  #: method to use for computing collisions
+    collision_metric: CollisionMetric = field(default_factory=lambda:CollisionMetric.discs)  #: method to use for computing collisions
     offroad_threshold: float = 0.5  #: how much the agents can go off-road without counting that as infraction
     left_handed_coordinates: bool = False  #: whether the coordinate system is left-handed (z always points upwards)
 
