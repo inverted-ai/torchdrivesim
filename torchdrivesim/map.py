a2fe92787c61693b96e871d555a223dc19065537
Adam Scibior <adam.scibior@inverted.ai>
2024-05-28 17:17:21 -0700

    Add a check for wrong way stoplines

--- torchdrivesim/map.py
+++ torchdrivesim/map.py
@@ -4,13 +4,16 @@ import os
 from dataclasses import dataclass
 from typing import Optional, Tuple, Dict, List
 
+import lanelet2
+import numpy as np
 import torch
 
 import torchdrivesim
-from torchdrivesim.lanelet2 import LaneletMap, load_lanelet_map, road_mesh_from_lanelet_map, lanelet_map_to_lane_mesh
+from torchdrivesim.lanelet2 import LaneletMap, find_lanelet_directions, load_lanelet_map, road_mesh_from_lanelet_map, lanelet_map_to_lane_mesh
 from torchdrivesim.mesh import BirdviewMesh
 from torchdrivesim.traffic_controls import BaseTrafficControl, TrafficLightControl, StopSignControl, YieldControl
 from torchdrivesim.traffic_lights import TrafficLightController
+from torchdrivesim.utils import normalize_angle
 
 
 @dataclass
@@ -224,3 +227,16 @@ def traffic_controls_from_map_config(cfg: MapConfig) -> Dict[str, BaseTrafficCon
             torch.stack(traffic_control_poses['yield_sign']).unsqueeze(0)
         )
     return traffic_controls
+
+
+def find_wrong_way_stoplines(map_cfg: MapConfig) -> List[int]:
+    wrong_way_stopline_ids = []
+    lanelet_map = map_cfg.lanelet_map
+    if lanelet_map is None:
+        return []
+    stoplines = map_cfg.stoplines
+    for stopline in stoplines:
+        lanelet_orientations = find_lanelet_directions(lanelet_map, stopline.x, stopline.y, lanelet_dist_tolerance=0)
+        if lanelet_orientations and not any([normalize_angle(psi - stopline.orientation) < np.pi / 2 for psi in lanelet_orientations]):
+            wrong_way_stopline_ids.append(stopline.actor_id)
+    return wrong_way_stopline_ids
