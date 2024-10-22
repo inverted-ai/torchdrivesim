4880db77415af18b87935b67776b2b66893a2ac9
Adam Scibior <adam.scibior@inverted.ai>
2024-10-22 16:31:34 -0700

    Fix DummyRenderer interface

--- torchdrivesim/rendering/base.py
+++ torchdrivesim/rendering/base.py
@@ -615,7 +615,7 @@ class DummyRenderer(BirdviewRenderer):
     """
     Produces a black image of the required size. Mostly used for debugging and benchmarking.
     """
-    def render_mesh(self, mesh: BirdviewMesh, res: Resolution, cameras: Cameras) -> Tensor:
+    def render_rgb_mesh(self, mesh: RGBMesh, res: Resolution, cameras: Cameras) -> Tensor:
         camera_batch_size = cameras.get_camera_center().shape[0]
         shape = (camera_batch_size, res.height, res.width, 3)
         image = torch.zeros(shape, device=self.device, dtype=torch.float32)
